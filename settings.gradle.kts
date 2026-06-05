428a36dc9e4f6a10e1ca5ab776ce50edfcd6620b
Hadi <xhopeter@proton.me>
2026-06-05 20:17:03 +0330

    fix: use myket maven mirror instead of google/maven-central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,13 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+        maven { url = uri("https://maven.myket.ir/") }
     }
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
+        maven { url = uri("https://maven.myket.ir/") }
     }
 }
 
