4d1d6ff3cbbc4655eb8c45eb63eeb77395c2b512
Hadi <xhopeter@proton.me>
2026-06-05 20:32:43 +0330

    fix: use aliyun maven mirrors instead of myket

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,24 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.myket.ir/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.myket.ir/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+    }
+}
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://gradle.chrepo.ir/") }
     }
 }
 
